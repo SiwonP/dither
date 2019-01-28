fe708ffb77d483d3de8a8f6ae6c164960ee764fc
Simon Petit <nomisp96@hotmail.fr>
2019-01-28 20:23:46 +0100

    MAJOR Functional program

--- src/dither.cc
+++ src/dither.cc
@@ -4,8 +4,24 @@
 #include "floyd_steinberg.h"
 #include "jjn.h"
 
+using namespace cv;
+
 int main(int argc, char *argv[])
 {
+    Mat image;
 
-    return 1;
+    if (argc < 3) {
+        printf("Usage: dither <input_file> <output_file>\n");
+    } else {
+
+
+    image = imread(argv[1], 0);
+
+    floyd_steinberg(image);
+
+    imwrite(argv[2], image);
+    }
+    image.release();
+
+    return 0;
 }
